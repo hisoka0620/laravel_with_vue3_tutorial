bbfd64f8bd4ba9579229425989b81d517cbedac2
hisoka0620 <kentaueno1995@icloud.com>
2021-06-03 03:27:33 +0900

    課題3の一回目の提出

--- resources/js/components/TaskCompleteComponent.vue
+++ resources/js/components/TaskCompleteComponent.vue
@@ -3,55 +3,72 @@
         <table class="table table-hover">
             <thead class="thead-light">
                 <tr>
-                    <th scope="col">#</th>
                     <th scope="col">Title</th>
                     <th scope="col">Content</th>
                     <th scope="col">Person In Charge</th>
-                    <th scope="col">Deadline</th>
-                    <th scope="col">Show</th>
-                    <th scope="col">Edit</th>
+                    <th scope="col" :class="addClass('deadline')" @click="sortBy('deadline')">Deadline</th>
+                    <th scope="col" :class="addClass('created_at')" @click="sortBy('created_at')">Created_at</th>
                     <th scope="col">Cancellation</th>
                 </tr>
             </thead>
             <tbody>
                 <tr v-for="(task,index) in tasks" :key="index">
-                    <th scope="row">{{ task.id }}</th>
-                    <td>{{ task.title }}</td>
+                    <td>
+                        <router-link
+                            :to="{ name: 'task.show', params: { taskId: task.id } }"
+                        >{{ task.title }}</router-link>
+                    </td>
                     <td>{{ task.content }}</td>
                     <td>{{ task.person_in_charge }}</td>
                     <td :class="dangerClass(task.deadline)">{{ task.deadline }}</td>
-                    <td>
-                        <router-link :to="{ name: 'task.show', params: { taskId: task.id } }">
-                            <button class="btn btn-primary">Show</button>
-                        </router-link>
-                    </td>
-                    <td>
-                        <router-link :to="{ name: 'task.edit', params: { taskId: task.id } }">
-                            <button class="btn btn-success">Edit</button>
-                        </router-link>
-                    </td>
+                    <td>{{ createTime(task.created_at) }}</td>
                     <td>
                         <button class="btn btn-danger" @click="cancelComplete(task.id)">Cancellation</button>
                     </td>
                 </tr>
             </tbody>
         </table>
+
+        <nav aria-label="..." v-show="show">
+            <ul class="pagination justify-content-center">
+                <li class="page-item" :class="{disabled: currentPage == 1}">
+                    <a class="page-link" href="#" tabindex="-1" @click="getTasks(currentPage - 1)">Previous</a>
+                </li>
+                <li v-for="(page, index) in pages" :key="index" class="page-item" :class="{active: page == currentPage}">
+                    <a href="#" class="page-link" @click="getTasks(page)">{{ page }}</a>
+                </li>
+                <li class="page-item" :class="{disabled: currentPage == lastPage}">
+                    <a class="page-link" href="#" @click="getTasks(currentPage + 1)">Next</a>
+                </li>
+            </ul>
+        </nav>
+
     </div>
 </template>
 
  <script>
+import moment from 'moment-timezone'
+
 export default {
-    data: function() {
+    data() {
         return {
             tasks: [],
-            hasClass: false
+            hasClass: false,
+            pageCount: '',
+            currentPage: 1,
+            lastPage: null,
+            sort_key: "",
+            sort_asc: true
         }
     },
     methods: {
-        getTasks() {
-            axios.get('/api/tasks?completed=true')
+        getTasks(pageNum) {
+            axios.get('/api/tasks?completed=true&page=' + pageNum)
                 .then((res) => {
-                    this.tasks = res.data
+                    this.tasks = res.data.data
+                    this.pageCount = res.data.last_page
+                    this.currentPage = res.data.current_page
+                    this.lastPage = res.data.last_page
                 })
         },
         cancelComplete(id) {
@@ -61,10 +78,49 @@ export default {
                 .then((res) => {
                     this.getTasks();
                 });
+        },
+        createTime(val){
+            return moment(val).tz('Asia/Tokyo').format('YYYY-MM-DD HH:mm:ss')
+        },
+        sortBy(key) {
+            this.sort_key === key ? (this.sort_asc = !this.sort_asc) : (this.sort_asc = true)
+            this.sort_key = key
+        },
+        addClass(key){
+            return {
+                asc: this.sort_key === key && this.sort_asc,
+                desc: this.sort_key === key && !this.sort_asc
+            }
+        }
+    },
+    computed: {
+        pages(){
+            return this.pageCount
+        },
+        show(){
+            return this.tasks.length > 0 ? true : false
+        },
+        tasks() {
+            if (this.sort_key !== "") {
+                let set = 1
+                this.sort_asc ? (set = -1) : (set = 1)
+                this.tasks.sort((a, b) => {
+                    if (a[this.sort_key] < b[this.sort_key]) {
+                        return -1 * set
+                    }
+                    if (a[this.sort_key] > b[this.sort_key]) {
+                        return 1 * set
+                    }
+                    return 0
+                })
+                return this.tasks
+            } else {
+                return this.tasks
+            }
         }
     },
     mounted() {
-        this.getTasks()
+        this.getTasks();
     }
 }
 </script>
--- resources/js/components/TaskExpiredComponent.vue
+++ resources/js/components/TaskExpiredComponent.vue
@@ -3,64 +3,116 @@
         <table class="table table-hover">
             <thead class="thead-light">
                 <tr>
-                    <th scope="col">#</th>
                     <th scope="col">Title</th>
                     <th scope="col">Content</th>
                     <th scope="col">Person In Charge</th>
-                    <th scope="col">Deadline</th>
-                    <th scope="col">Show</th>
-                    <th scope="col">Edit</th>
-                    <th scope="col">Cancellation</th>
+                    <th scope="col" :class="addClass('deadline')" @click="sortBy('deadline')">Deadline</th>
+                    <th scope="col" :class="addClass('created_at')" @click="sortBy('created_at')">Created_at</th>
+                    <th scope="col">Delete</th>
                 </tr>
             </thead>
             <tbody>
                 <tr v-for="(task,index) in tasks" :key="index">
-                    <th scope="row">{{ task.id }}</th>
-                    <td>{{ task.title }}</td>
+                    <router-link :to="{ name: 'task.show', params: { taskId: task.id } }">
+                        <td>{{ task.title }}</td>
+                    </router-link>
                     <td>{{ task.content }}</td>
                     <td>{{ task.person_in_charge }}</td>
                     <td :class="dangerClass(task.deadline)">{{ task.deadline }}</td>
+                    <td>{{ createTime(task.created_at) }}</td>
                     <td>
-                        <router-link :to="{ name: 'task.show', params: { taskId: task.id } }">
-                            <button class="btn btn-primary">Show</button>
-                        </router-link>
-                    </td>
-                    <td>
-                        <router-link :to="{ name: 'task.edit', params: { taskId: task.id } }">
-                            <button class="btn btn-success">Edit</button>
-                        </router-link>
-                    </td>
-                    <td>
-                        <button class="btn btn-danger" @click="cancelComplete(task.id)">Cancellation</button>
+                        <button class="btn btn-danger" @click="deleteExpired(task.id)">Delete</button>
                     </td>
                 </tr>
             </tbody>
         </table>
+
+        <nav aria-label="..." v-show="show">
+            <ul class="pagination justify-content-center">
+                <li class="page-item" :class="{disabled: currentPage == 1}">
+                    <a class="page-link" href="#" tabindex="-1" @click="getTasks(currentPage - 1)">Previous</a>
+                </li>
+                <li v-for="(page, index) in pages" :key="index" class="page-item" :class="{active: page == currentPage}">
+                    <a href="#" class="page-link" @click="getTasks(page)">{{ page }}</a>
+                </li>
+                <li class="page-item" :class="{disabled: currentPage == lastPage}">
+                    <a class="page-link" href="#" @click="getTasks(currentPage + 1)">Next</a>
+                </li>
+            </ul>
+        </nav>
+
     </div>
 </template>
 
  <script>
+import moment from 'moment-timezone'
+
 export default {
     data: function() {
         return {
             tasks: [],
-            hasClass: false
+            hasClass: false,
+            pageCount: '',
+            currentPage: 1,
+            lastPage: null,
+            sort_key: "",
+            sort_asc: true
         }
     },
     methods: {
-        getTasks() {
-            axios.get('/api/tasks?deadline=true')
+        getTasks(pageNum) {
+            axios.get('/api/tasks?deadline=true&page=' + pageNum)
                 .then((res) => {
-                    this.tasks = res.data
+                    this.tasks = res.data.data
+                    this.pageCount = res.data.last_page
+                    this.currentPage = res.data.current_page
+                    this.lastPage = res.data.last_page
                 })
         },
-        cancelComplete(id) {
-            axios.put(`/api/tasks/${id}`, {
-                confirmed: 0
-            })
+        deleteExpired(id) {
+            axios.delete(`/api/tasks/${id}`)
                 .then((res) => {
                     this.getTasks();
                 });
+        },
+        createTime(val){
+            return moment(val).tz('Asia/Tokyo').format('YYYY-MM-DD HH:mm:ss')
+        },
+        sortBy(key) {
+            this.sort_key === key ? (this.sort_asc = !this.sort_asc) : (this.sort_asc = true)
+            this.sort_key = key
+        },
+        addClass(key){
+            return {
+                asc: this.sort_key === key && this.sort_asc,
+                desc: this.sort_key === key && !this.sort_asc
+            }
+        }
+    },
+    computed: {
+        pages(){
+            return this.pageCount
+        },
+        show(){
+            return this.tasks.length > 0 ? true : false
+        },
+        tasks() {
+            if (this.sort_key !== "") {
+                let set = 1
+                this.sort_asc ? (set = -1) : (set = 1)
+                this.tasks.sort((a, b) => {
+                    if (a[this.sort_key] < b[this.sort_key]) {
+                        return -1 * set
+                    }
+                    if (a[this.sort_key] > b[this.sort_key]) {
+                        return 1 * set
+                    }
+                    return 0
+                })
+                return this.tasks
+            } else {
+                return this.tasks
+            }
         }
     },
     mounted() {
